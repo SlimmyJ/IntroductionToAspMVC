641a48614dbba94bc69954dd6f1589293ac92483
Michiel Wouters <michiel_woute64@hotmail.com>
2021-05-25 11:51:32 +0200

    Add project files.

---
+++ IntroductionToAspMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IntroductionToAspMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
